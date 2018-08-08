28871fd0ed52366faf61b99d521da6e8aa29db5f
Jacobus <jbrink386@gmail.com>
2018-08-08 08:20:01 +0200

    Update Install.bat
    - Added hostName default parameter

--- Install.bat
+++ Install.bat
@@ -1,2 +1,2 @@
-powershell "& '%~dp0\Install.ps1' -overwriteIfExist $false -iisAppPoolName myWebAppPool -iisAppPoolDotNetVersion v4.0 -iisAppName myWebApp"
+powershell "& '%~dp0\Install.ps1' -overwriteIfExist $false -iisAppPoolName myWebAppPool -iisAppPoolDotNetVersion v4.0 -iisAppName myWebApp -hostName localhost"
 pause
